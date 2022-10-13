b3b811a8c551202b7ee3505032bc4063eb2a631b
RicardoArquimedes <richardcqt28@gmail.com>
2022-10-13 17:33:29 -0500

    :sparkles: adding files for ES8; async functions, object entries & values, string padding and trailing commas

---
+++ src/ES8/00-object-entries.js
@@ -0,0 +1,3 @@
+const countries = {MX: 'Mexico', C0: 'Colombia', PE: 'Perú'};
+
+console.log(Object.entries(countries));
---
+++ src/ES8/03-string-padding.js
@@ -0,0 +1,3 @@
+const string = 'Hello!';
+console.log(string.padStart(8, 'hi'))
+console.log(string.padEnd(9, 'hi'))
